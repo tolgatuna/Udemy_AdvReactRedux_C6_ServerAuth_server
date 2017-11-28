e4b72419e0ac6b0fd31ab4c06ce142026e1c5247
tolga.tuna <tolga.tuna@hotmail.com>
2017-11-28 03:45:46 -0800

    [Udemy Advanced React & Redux Chapter 6 - Server Setup & Authentication] server codes initail commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// Main starting point of the application
+// Import in ES5
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express();      // We create an instance of express
+
+// ## APP SETUP ##
+// Middleware of express declarations :
+// Any incoming request will pass into morgan and bodyParser
+app.use(morgan('combined'));                // Morgan: Logging framework (using for debugging)
+app.use(bodyParser.json({type: '*/*'}));    // bodyParser: use for parse incoming request and ve say parse coming request as json
+
+// ## SERVER SETUP ##
+const port = process.env.PORT || 3090;  // Port setup
+const server = http.createServer(app);  // Create an http server with express
+server.listen(port);                    // Give port of server
+console.log('Server is listening port:', port);
