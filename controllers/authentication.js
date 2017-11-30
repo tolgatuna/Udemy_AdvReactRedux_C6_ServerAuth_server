328e7149d2e6a5af1ebd01586895d410fc187c65
tolgatuna <tolga.tuna@hotmail.com>
2017-11-30 17:52:26 +0300

    70. Searching for users part is completed.

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,3 +1,24 @@
+const User = require('../models/user');
+
 exports.signup = function (req, res, next) {
-    res.send({success:true})
+
+
+    const email = req.body.email;
+    const password = req.body.password;
+    console.log('Email    : ', email);
+    console.log('Password : ', password);
+
+    // See if a user with the given email exists
+    User.findOne({email: email}, function(error, existingUser) {
+
+    });
+
+    // If a user with given email does exist, return an error
+
+
+    // If a user with given email does NOT exist, create and save user record
+
+
+    // Respond to request indicating user was created
+
 };
