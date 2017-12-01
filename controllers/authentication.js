58b85aa433f92dd51261f8926f5839ec47258633
tolgatuna <tolga.tuna@hotmail.com>
2017-12-01 17:04:53 +0300

    71. Creating User Records part is completed.

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -2,23 +2,49 @@ const User = require('../models/user');
 
 exports.signup = function (req, res, next) {
 
-
     const email = req.body.email;
     const password = req.body.password;
+
+    if(!email || !password) {
+        return res.status(422).send({error: 'You must provide an email and a password'});
+    }
+
     console.log('Email    : ', email);
     console.log('Password : ', password);
 
     // See if a user with the given email exists
-    User.findOne({email: email}, function(error, existingUser) {
+    User.findOne({email: email}, function(err, existingUser) {
+        if(err) {
+            return next(err);
+        }
 
+        // If a user with given email does exist, return an error
+        if(existingUser) {
+            return res.status(422).send({error: 'Email is in use'}); // 422: not processable entity
+        }
+
+        // If a user with given email does NOT exist, create and save user record
+        const user = new User({
+            email: email,
+            password: password
+        }); // Create a new user
+
+        // And save the database
+        user.save(function (err) {
+            if(err) {
+                return next(err);
+            }
+
+            // Respond to request indicating user was created
+            return res.json({success: true});
+        });
     });
 
-    // If a user with given email does exist, return an error
 
 
-    // If a user with given email does NOT exist, create and save user record
 
 
-    // Respond to request indicating user was created
+
+
 
 };
