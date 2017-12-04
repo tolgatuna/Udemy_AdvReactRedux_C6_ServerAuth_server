28e9c39e080ab2eea9f4d21c5a6cb6e3dcc54e9e
tolgatuna <tolga.tuna@hotmail.com>
2017-12-04 11:58:38 +0300

    73. Salting a password part is completed.

--- models/user.js
+++ models/user.js
@@ -9,21 +9,26 @@ const userSchema = new Schema({
 });
 
 // On Save Hook, encrypt password
+// Before saving a modal, run this function
 userSchema.pre('save', function (next) {
-    const user = this;
+    // get access to the user model
+    const user = this;  //user.email, user.password
 
+    // generate a salt then run callback (function (err, salt))
     bcrypt.genSalt(10, function (err, salt) {
         if(err) {
             return next(err);
         }
 
+        // After generate salt, hash (encrypt) our password using the generated salt
         bcrypt.hash(user.password, salt, null, function (err, hash) {
            if(err) {
                return next(err);
            }
 
+           // Overwrite our plain text user password with hashed (encrypted) password
            user.password = hash;
-           next();
+           next();  // Go ahead and do save operation after all
         });
     });
 });
