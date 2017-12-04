448162d53523d2a693a055a5860f2faf435b5698
tolgatuna <tolga.tuna@hotmail.com>
2017-12-04 17:35:36 +0300

    78. Using Strategies with Passport part is completed.

--- services/passport.js
+++ services/passport.js
@@ -5,7 +5,10 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 
 // Set up options for JWT Strategy
-const jwtOptions = {};
+const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+    secretOrKey: config.secret
+};
 
 
 // Crate JWT Strategy
@@ -28,3 +31,4 @@ const jwtLogin = new JwtStrategy(jwtOptions, function (payload, done) {
 
 
 // Tell passport to use this strategy
+passport.use(jwtLogin);
