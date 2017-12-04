8633976fd7ef5a595c7ab75990955d1029463c5e
tolgatuna <tolga.tuna@hotmail.com>
2017-12-04 17:45:02 +0300

    79. Making an Authenticated Request part is completed. Dummy route is added.

--- router.js
+++ router.js
@@ -1,7 +1,13 @@
 const authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', {session: false});
 
 module.exports = function (app) {
+    app.get('/dummy', requireAuth, function (req, res) {
+        res.send({hi: "there"});
+    });
 
-    app.post('/signup', authentication.signup)
-
+    app.post('/signup', authentication.signup);
 };
